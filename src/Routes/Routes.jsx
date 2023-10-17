5ab8f29fd72fdd322f0f34d14cf92867107ecbcf
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-18 00:48:13 +0600

    Navbar section added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,17 @@
 
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
 
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element:<div>Hello</div>
+        element:<Home></Home>,
+        children:[
+            {
+                path:'/addProduct',
+            }
+        ]
     }
 ])
 
